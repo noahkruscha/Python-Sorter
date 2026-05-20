dc8603fcf0830a77ccd5062ff55061cd58c09bcc
noahkruscha <noahkruscha@gmail.com>
2026-05-20 21:28:58 +0200

    Refactor watcher.py to include main guard
    Added main guard to run the observer and log PID.

--- code/watcher.py
+++ code/watcher.py
@@ -1,31 +1,35 @@
+import os
+import sys
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import config_paths as cp
 import sorter
 import time
 
-# Event Handler
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
-        
         file_path = event.src_path
         time.sleep(0.5)
         sorter.sort_file(file_path)
-        
 
-# observer starten
-observer = Observer()
-event_handler = Handler()
-observer.schedule(event_handler, cp.EVENT_DIR, recursive=False)
-observer.start()
+if __name__ == "__main__":
+    pid_file = cp.LOG_PATH / "watcher.pid"
+    pid_file.write_text(str(os.getpid()))
 
-try:
-    while True:
-        time.sleep(1)
+    print(f"Watching: {cp.EVENT_DIR}")  # <- existiert der Pfad?
+    print(f"PID: {os.getpid()}")
 
-except KeyboardInterrupt:
-    observer.stop()
+    observer = Observer()
+    event_handler = Handler()
+    observer.schedule(event_handler, cp.EVENT_DIR, recursive=False)
+    observer.start()
 
-observer.join()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
