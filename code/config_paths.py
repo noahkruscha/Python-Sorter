6b95b3e9a308b21e3b20908fe5774fa8721eac4d
noahkruscha <noahkruscha@gmail.com>
2026-05-20 21:31:30 +0200

    Set TKDND_LIBRARY environment variable
    Added environment variable for TKDND_LIBRARY path.

--- code/config_paths.py
+++ code/config_paths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -7,3 +8,5 @@ OUTPUT_DIR = BASE_DIR.parent / "result" / "organized"
 CONFIG_PATH = BASE_DIR / "rules.json"
 SETTINGS_PATH = BASE_DIR / "settings.json"
 LOG_PATH = BASE_DIR.parent / "result" / "logs"
+
+os.environ['TKDND_LIBRARY'] = str(BASE_DIR.parent / 'python_env' / 'libs' / 'tkinterdnd2' / 'tkdnd')
